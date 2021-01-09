e5a4f4eeee41ff15a809ca3b256d54246209b262
LarsvanDartel <larsvandartel73@gmail.com>
2021-01-09 15:51:39 +0000

    fixed channel assignment

--- functions.js
+++ functions.js
@@ -16,10 +16,6 @@ const getChessChannel = function(guild){
     const chessChannel = guild.channels.cache.find(channel => channel.name.toLowerCase().includes('chess'));
     if(chessChannel) return chessChannel;
 
-    if(guild.channels.cache.has(guild.id)) return guild.channels.cache.get(guild.id);
-
-    return guild.channels.cache.filter(c => c.type === "text" && c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-        .sort((a, b) => a.position - b.position || Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-        .first();
+    return getGeneralChannel(guild);
 }
 module.exports = {getGeneralChannel, getChessChannel};
